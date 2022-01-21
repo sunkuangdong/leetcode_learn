103c0d041b76b98a0ac75651019adc37fc881497
sunkuangdong <sunkuangdong@edianzu.com>
2022-01-21 16:43:23 +0800

    队列的最大值

--- array/queue/index3.js
+++ array/queue/index3.js
@@ -1,57 +1,38 @@
-// 队列的最大值
 var MaxQueue = function () {
-    this.myQueue = {}
-    this.head = null
-    this.rear = null
-    this.maxValue = null
+    this.queue = []
+    this.maxQueue = -1
 };
 
 /**
  * @return {number}
  */
 MaxQueue.prototype.max_value = function () {
-    return this.maxValue ? this.maxValue : -1
+    return this.maxQueue
 };
 
-/** 
+/**
  * @param {number} value
  * @return {void}
  */
 MaxQueue.prototype.push_back = function (value) {
-    if (!this.myQueue.value) {
-        this.myQueue = {
-            value,
-            next: null
-        }
-        this.rear = this.myQueue
+    this.queue.push(value)
+    if (this.queue.length === 1) {
+        this.maxQueue = value
     } else {
-        // 存储值
-        this.maxValue = Math.max(this.rear.value, value)
-        this.rear.next = {
-            value,
-            next: null
-        }
-        this.rear = this.rear.next
+        this.maxQueue = Math.max(this.maxQueue, value)
     }
-    this.head = this.myQueue
 };
 
 /**
  * @return {number}
  */
 MaxQueue.prototype.pop_front = function () {
-    const value = this.head.value ? this.head.value : -1
-    this.head = this.myQueue.next
-    this.myQueue = !this.head ? {} : this.head
-    return value
+    if (!this.queue.length) return -1
+    const popNum = this.queue.shift()
+    // Math.max(...this.queue) 这步可能不是O(1)
+    this.maxQueue = this.queue.length ? Math.max(...this.queue) : -1
+    return popNum
 };
-
-const obj = new MaxQueue()
-obj.push_back(1)
-obj.push_back(2)
-obj.max_value()
-let param_1 = obj.pop_front()
-console.dir(param_1)
 
 /**
  * Your MaxQueue object will be instantiated and called as such:
